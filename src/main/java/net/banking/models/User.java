c65f2433dcb5f22ae722c1c27ce5904883d8ecf7
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-11-20 22:16:06 +0200

    completed user model

---
+++ src/main/java/net/banking/models/User.java
@@ -0,0 +1,92 @@
+package net.banking.models;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime dateCreated;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id){
+        if(id <= 0)
+            throw new IllegalArgumentException("invalid id supplied");
+
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if(firstName == null || firstName.trim().length() == 0)
+            throw new IllegalArgumentException("invalid firstName supplied");
+
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if(lastName == null || lastName.trim().length() == 0)
+            throw new IllegalArgumentException("invalid lastName supplied");
+
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if(email == null || email.trim().length() == 0 || !email.contains("@"))
+            throw new IllegalArgumentException("invalid email supplied");
+
+        this.email = email;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        if(dateCreated == null)
+            throw new IllegalArgumentException("invalid dateCreated supplied");
+
+        this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{ \n" +
+                " id=" + id +
+                ", \n firstName='" + firstName + '\'' +
+                ", \n lastName='" + lastName + '\'' +
+                ", \n email='" + email + '\'' +
+                ", \n dateCreated=" + dateCreated +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(! (obj instanceof User))
+            return false;
+
+        User user = (User)obj;
+
+        return this.id == user.id && this.email.equals(user.email) && this.dateCreated == user.dateCreated;
+    }
+
+    @Override
+    public int hashCode(){
+        return id;
+    }
+}
