4ffd7b141b3f3e65eab8c1f9a837aafa6cf369c4
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-11-26 10:20:51 +0200

    heroku depe

--- src/main/java/net/banking/App.java
+++ src/main/java/net/banking/App.java
@@ -10,14 +10,30 @@ import net.banking.service.UserService;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 public class App
 {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main( String[] args )
     {
+
+        port(getHerokuAssignedPort());
+
         staticFiles.location("/public");
 
         new Controller(new UserService(), new AccountService(), new TransactionService());
     }
 }
+
+
+
